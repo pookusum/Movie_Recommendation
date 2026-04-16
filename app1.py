491b7c03a0170d221a7cfc52f09a34956b6ee607
Pooja <kusumpooja18@gmail.com>
2026-04-16 19:02:14 +0530

    Updated app with gdown fix

--- app1.py
+++ app1.py
@@ -1,46 +1,63 @@
-import pickle 
+import pickle
 import pandas as pd
-import streamlit as st 
+import streamlit as st
+import os
+import gdown
 
+# ---------------------------
+# Download PKL files only if missing
+# ---------------------------
+MOVIE_DICT_URL = "https://drive.google.com/uc?id=YOUR_MOVIE_DICT_FILE_ID"
+SIMILARITY_URL = "https://drive.google.com/uc?id=YOUR_SIMILARITY_FILE_ID"
 
-data = pickle.load(open('movie_dict.pkl', mode='rb'))
+if not os.path.exists("movie_dict.pkl"):
+    gdown.download(MOVIE_DICT_URL, "movie_dict.pkl", quiet=False)
+
+if not os.path.exists("similarity.pkl"):
+    gdown.download(SIMILARITY_URL, "similarity.pkl", quiet=False)
+
+# ---------------------------
+# Load data
+# ---------------------------
+data = pickle.load(open("movie_dict.pkl", "rb"))
 data = pd.DataFrame(data)
-#print(movies) 
 
-#s
+similarity = pickle.load(open("similarity.pkl", "rb"))
 
-similarity = pickle.load(open('similarity.pkl', mode='rb'))
-print(similarity)
-
-#Final function
+# ---------------------------
+# Recommendation function
+# ---------------------------
 def recommend(movie):
-
-
     recommended_movies = []
 
-    movie_index = data[data['title']== movie].index[0]
+    movie_index = data[data['title'] == movie].index[0]
     distance = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distance)), reverse= True, key= lambda x: x[1])[1:6]
+
+    movie_list = sorted(
+        list(enumerate(distance)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
 
     for i in movie_list:
-      #  print(i[0])
-      print(data.iloc[i[0]].title)
+        recommended_movies.append(data.iloc[i[0]].title)
 
-    return recommended_movies      
+    return recommended_movies
 
 
-st.title('Movie Recommendation System')
+# ---------------------------
+# Streamlit UI
+# ---------------------------
+st.title("Movie Recommendation System")
 
-import streamlit as st
+selected_movie = st.selectbox(
+    "Select a movie to get a recommendation",
+    data['title'].values
+)
 
-Selected_Movie = st.selectbox("Select a movie to get a recommendation", data['title'].values)
-btn=st.button('Recommend')
+if st.button("Recommend"):
+    list_of_movies = recommend(selected_movie)
 
-if btn:
-   
-    list_of_movies= recommend(Selected_Movie)
-     
+    st.subheader("Recommended Movies:")
     for movie in list_of_movies:
-      st.write(movie)
-      
-
+        st.success(movie)
